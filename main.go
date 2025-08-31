5f7dbfaeda0d182e5dac0a1b39ea26bcdd984890
hjy <hjy666666@sjtu.edu.cn>
2025-08-31 09:37:51 +0800

    finish redirect

--- main.go
+++ main.go
@@ -11,8 +11,15 @@ import (
 
 var long_to_short, short_to_long map[string]string
 
-func httpHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World")
+func hashRedirectHandler(w http.ResponseWriter, r *http.Request) {
+	short_url := r.URL.Path
+	original_url, ok := short_to_long[short_url[1:]]
+	if !ok {
+		log.Println("Error:Invalid short url", short_url)
+	} else {
+		http.Redirect(w, r, original_url, http.StatusFound)
+		log.Println("Redirecting to", original_url)
+	}
 }
 func hash_process_long_to_short(long_url string) string {
 	hash := md5.Sum([]byte(long_url))
@@ -45,6 +52,7 @@ func addUrl() {
 			if ok {
 				delete(long_to_short, long_url)
 				delete(short_to_long, short_url)
+				log.Println("Delete url:", long_url)
 			} else {
 				log.Println("long url not exists:", long_url)
 			}
@@ -58,7 +66,7 @@ func main() {
 	short_to_long = make(map[string]string)
 	long_to_short = make(map[string]string)
 	go addUrl()
-	http.HandleFunc("/", httpHandler)
+	http.HandleFunc("/", hashRedirectHandler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 
