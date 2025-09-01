4ee639dfd23efc058ef8eada66585d0c90a35970
hjy <hjy666666@sjtu.edu.cn>
2025-09-01 14:55:32 +0800

    complete add and browse func

--- main.go
+++ main.go
@@ -3,25 +3,13 @@ package main
 import (
 	"crypto/md5"
 	"encoding/hex"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
-	"os"
 )
 
 var long_to_short, short_to_long map[string]string
 
-func hashRedirectHandler(w http.ResponseWriter, r *http.Request) {
-	short_url := r.URL.Path
-	original_url, ok := short_to_long[short_url[1:]]
-	if !ok {
-		log.Println("Error:Invalid short url", short_url)
-	} else {
-		http.Redirect(w, r, original_url, http.StatusFound)
-		log.Println("Redirecting to", original_url)
-	}
-}
 func hash_process_long_to_short(long_url string) string {
 	hash := md5.Sum([]byte(long_url))
 	res := hex.EncodeToString(hash[:])
@@ -38,41 +26,6 @@ func addUrl(long_url string) {
 		log.Println("add url:", long_url, "->", short_url)
 	}
 }
-func cmdProcessor() {
-	for {
-		var order, long_url string
-		fmt.Scan(&order)
-		if order == "exit" {
-			os.Exit(0)
-		}
-		if order == "add" {
-			fmt.Scan(&long_url)
-			short_url, ok := long_to_short[long_url]
-			if ok {
-				log.Println("short url already exists:", short_url)
-			} else {
-				short_url = hash_process_long_to_short(long_url)
-				long_to_short[long_url] = short_url
-				short_to_long[short_url] = long_url
-				log.Println("add url:", long_url, "->", short_url)
-			}
-			continue
-		}
-		if order == "del" {
-			fmt.Scan(&long_url)
-			short_url, ok := long_to_short[long_url]
-			if ok {
-				delete(long_to_short, long_url)
-				delete(short_to_long, short_url)
-				log.Println("Delete url:", long_url)
-			} else {
-				log.Println("long url not exists:", long_url)
-			}
-			continue
-		}
-		log.Println("order not exists:", order)
-	}
-}
 func getLongUrl(short_url string) (string, bool) {
 	long_url, ok := short_to_long[short_url]
 	return long_url, ok
@@ -85,6 +38,10 @@ func PostHandler(c *gin.Context) {
 	url := c.PostForm("url")
 	if option == "add" {
 		addUrl(url)
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"OriginalUrl": url,
+			"Result":      long_to_short[url],
+		})
 	} else if option == "browse" {
 		original_url, ok := getLongUrl(url)
 		if !ok {
