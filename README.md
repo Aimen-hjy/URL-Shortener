1094c58bee5117738ebc758f1f7f171d085965e2
hjy <hjy666666@sjtu.edu.cn>
2025-09-01 20:36:36 +0800

    add database haven't debug

--- README.md
+++ README.md
@@ -4,3 +4,13 @@
 - [x] V1-基本功能：生成短链接，用内存存储映射（map），重定向到原始URL
 - [x] V2-Web界面：学习使用Gin框架进行开发
 - [ ] V3-持久化存储：使用数据库存储短链接和原始URL的映射关系
+## 使用说明
+先在本地安装Go环境，安装Gin框架，SQlite3数据库驱动，然后直接运行main.go文件，访问http://localhost:8080
+```bash
+go get -u github.com/gin-gonic/gin
+go get github.com/mattn/go-sqlite3
+```
+## 参考仓库
+> 本项目使用了以下仓库
+> - [Gin框架](https://github.com/gin-gonic/gin)
+> - [SQLite3](https://github.com/sqlite/sqlite)
--- main.go
+++ main.go
@@ -2,51 +2,81 @@ package main
 
 import (
 	"crypto/md5"
+	"database/sql"
 	"encoding/hex"
 	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"net/http"
 )
 
-var long_to_short, short_to_long map[string]string
+var database *sql.DB
 
+func getLongByShort(short_url string) (string, bool, error) {
+	var long_url string
+	err := database.QueryRow("SELECT long_url FROM urls WHERE short_url=?", short_url).Scan(&long_url)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return "", false, nil
+		}
+		return "", false, err
+	}
+	return long_url, true, nil
+}
+func getShortByLong(long_url string) (string, bool, error) {
+	var short_url string
+	err := database.QueryRow("SELECT short_url FROM urls WHERE long_url=?", long_url).Scan(&short_url)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return "", false, nil
+		}
+		return "", false, err
+	}
+	return short_url, true, nil
+}
+func insertUrl(long_url, short_url string) error {
+	_, err := database.Exec("INSERT INTO URLmap (long_url, short_url) VALUES (?, ?)", long_url, short_url)
+	return err
+}
 func hash_process_long_to_short(long_url string) string {
 	hash := md5.Sum([]byte(long_url))
 	res := hex.EncodeToString(hash[:])
 	return res[:10]
 }
 func addUrl(long_url string) {
-	short_url, ok := long_to_short[long_url]
+	short_url, ok, err := getShortByLong(long_url)
+	if err != nil {
+		log.Fatal(err)
+	}
 	if ok {
 		log.Println("short url already exists:", short_url)
 	} else {
 		short_url = hash_process_long_to_short(long_url)
-		long_to_short[long_url] = short_url
-		short_to_long[short_url] = long_url
+		err = insertUrl(long_url, short_url)
+		if err != nil {
+			log.Fatal(err)
+		}
 		log.Println("add url:", long_url, "->", short_url)
 	}
 }
-func getLongUrl(short_url string) (string, bool) {
-	long_url, ok := short_to_long[short_url]
-	return long_url, ok
-}
+
 func mainHandler(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", gin.H{})
 }
 func PostHandler(c *gin.Context) {
 	option := c.PostForm("option")
 	url := c.PostForm("url")
+	true_url, _, _ := getShortByLong(url)
 	if option == "add" {
 		addUrl(url)
 		c.HTML(http.StatusOK, "index.html", gin.H{
 			"OriginalUrl": url,
-			"Result":      long_to_short[url],
+			"Result":      true_url,
 		})
 	} else if option == "browse" {
-		original_url, ok := getLongUrl(url)
+		original_url, ok, _ := getLongByShort(url)
 		if !ok {
 			c.HTML(http.StatusOK, "error.html", gin.H{})
-			//TODO: Add a page to show error
 		} else {
 			c.Redirect(http.StatusFound, original_url)
 		}
@@ -54,7 +84,7 @@ func PostHandler(c *gin.Context) {
 }
 func RedirectHandler(c *gin.Context) {
 	short_url := c.Param("short_url")
-	original_url, ok := getLongUrl(short_url)
+	original_url, ok, _ := getLongByShort(short_url)
 	if !ok {
 		c.HTML(http.StatusOK, "error.html", gin.H{})
 	} else {
@@ -62,8 +92,16 @@ func RedirectHandler(c *gin.Context) {
 	}
 }
 func main() {
-	short_to_long = make(map[string]string)
-	long_to_short = make(map[string]string)
+	var err error
+	database, err = sql.Open("sqlite3", "./map.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer database.Close()
+	_, err = database.Exec("CREATE TABLE IF NOT EXISTS URLmap (long_url TEXT PRIMARY KEY, short_url TEXT)")
+	if err != nil {
+		log.Fatal(err)
+	}
 	r := gin.Default()
 	r.LoadHTMLGlob("static/*")
 	r.GET("/", mainHandler)
